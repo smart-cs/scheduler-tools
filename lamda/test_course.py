40a0fed481bc308f29703d347a339af827214ddd
Nicholas Wu <nickwu241@gmail.com>
2017-08-09 09:14:34 -0700

    Add test_course.py

---
+++ lamda/test_course.py
@@ -0,0 +1,33 @@
+import unittest
+import requests
+
+import course
+
+class TestCourseModule(unittest.TestCase):
+    def test_append_perm(self):
+        result = course.append_perm([], ['a', 'b', 'c'])
+        expected = [['a'], ['b'], ['c']]
+        self.assertCountEqual(expected, result)
+        self.assertListEqual(expected, result)
+
+        result = course.append_perm([['a'], ['b'], ['c']], ['1', '2', '3', '4'])
+        expected = [['a', '1'], ['a', '2'], ['a', '3'], ['a', '4'],
+                    ['b', '1'], ['b', '2'], ['b', '3'], ['b', '4'],
+                    ['c', '1'], ['c', '2'], ['c', '3'], ['c', '4']]
+        self.assertCountEqual(expected, result)
+        self.assertListEqual(expected, result)
+
+class TestScheduleCreator(unittest.TestCase):
+    def setUp(self):
+        self.db = requests.get('https://ubc-coursedb.firebaseio.com/2017W.json').json()
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_create_from_course_names(self):
+        # result = course.ScheduleCreator.create_from_course_names(['MATH 100', 'GRSJ 230'], self.db)
+
+         
+if __name__ == '__main__':
+    unittest.main()
